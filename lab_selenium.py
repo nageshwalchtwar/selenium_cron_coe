9d69c6c7c5189493a402aff95ad6acf357f26ba0
nageshwalchtwar <95535586+nageshwalchtwar@users.noreply.github.com>
2023-08-25 18:47:28 +0530

    Update lab_selenium.py

--- lab_selenium.py
+++ lab_selenium.py
@@ -47,21 +47,22 @@ load_dotenv()
 total = 0
  # Set webdriver options
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+# chrome_options = Options()
+# options = [
+#     "--headless",
+#     "--disable-gpu",
+#     "--window-size=1920,1200",
+#     "--ignore-certificate-errors",
+#     "--disable-extensions",
+#     "--no-sandbox",
+#     "--disable-dev-shm-usage"
+# ]
+# for option in options:
+#     chrome_options.add_argument(option)
+# driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver = webdriver.Firefox()
 
 # Load the updated YAML file
 with open('lab_data.yaml', 'r') as file:
